a589a1455eddee94d690e101d26065ed28c2b339
yunozawa <yuria12290224@gmail.com>
2026-04-05 09:57:34 +0900

    ログアウト機能実装

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -10,7 +10,18 @@
 </head>
 <body>
     <header class="header">
-        <img src="{{ asset('logo.png') }}" alt="ロゴ" class="header__logo">
+    <img src="{{ asset('logo.png') }}" alt="ロゴ" class="header__logo">
+    @auth
+    <nav class="header__nav">
+        <a href="/attendance" class="header__link">勤怠</a>
+        <a href="/attendance/list" class="header__link">勤怠一覧</a>
+        <a href="/stamp_correction_request/list" class="header__link">申請</a>
+        <form method="POST" action="{{ route('logout') }}" class="header__logout-form">
+            @csrf
+            <button type="submit" class="header__link header__logout-button">ログアウト</button>
+        </form>
+    </nav>
+    @endauth
     </header>
     <main>
         @yield('content')
